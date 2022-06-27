59bd73ca126e9eecade4d6154deb9679f6bb9014
Anatolio <nikiforova693@gmail.com>
2022-06-27 15:43:32 +0000

    chore: installation script

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 
 curl https://github.com/forestvpn/cli/releases/download/latest/forest -o /usr/local/bin/forest
 curl https://github.com/forestvpn/cli/releases/download/latest/forestd -o /usr/local/bin/forestd
+curl https://raw.githubusercontent.com/forestvpn/cli/main/forestd/env/forestd.service -o /etc/systemd/system/forestd.service
+systemctl daemon-reload
+systemctl start forestd
+forest --help
