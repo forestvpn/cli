12aed81007a8daf0db15d554e30c5daa8dfa4183
Anatolio <nikiforova693@gmail.com>
2022-08-26 10:28:36 +0000

    fix: location not found

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -24,13 +24,13 @@ var (
 	// DSN is a Data Source Name for Sentry. It is stored in an environment variable and assigned during the build with ldflags.
 	//
 	// See https://docs.sentry.io/product/sentry-basics/dsn-explainer/ for more information.
-	DSN = os.Getenv("SENTRY_DSN")
+	DSN string
 	// appVersion value is stored in an environment variable and assigned during the build with ldflags.
 	appVersion string
 	// firebaseApiKey is stored in an environment variable and assigned during the build with ldflags.
-	firebaseApiKey = os.Getenv("STAGING_FIREBASE_API_KEY")
+	firebaseApiKey string
 	// ApiHost is a hostname of Forest VPN back-end API that is stored in an environment variable and assigned during the build with ldflags.
-	ApiHost = os.Getenv("STAGING_API_URL")
+	ApiHost string
 )
 
 func main() {
