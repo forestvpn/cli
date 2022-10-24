59926164e9a3bd043cfaa95c626999ddcd2a028f
Anatolio <nikiforova693@gmail.com>
2022-10-24 12:42:37 +0000

    chore(ci): build variables

--- src/actions/main.go
+++ src/actions/main.go
@@ -19,9 +19,9 @@ import (
 
 var (
 	// firebaseApiKey is stored in an environment variable and assigned during the build with ldflags.
-	firebaseApiKey = os.Getenv("STAGING_FIREBASE_API_KEY")
+	firebaseApiKey string
 	// ApiHost is a hostname of Forest VPN back-end API that is stored in an environment variable and assigned during the build with ldflags.
-	apiHost = os.Getenv("STAGING_API_URL")
+	apiHost string
 )
 
 // AuthClientWrapper is a structure that is used as a high-level wrapper for both AuthClient and ApiClient.
