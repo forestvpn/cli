4100d7331b387e3e099d3bdf9a4d41c3e54405d5
Anatolio <nikiforova693@gmail.com>
2022-10-25 16:09:23 +0000

    fixex

--- src/main.go
+++ src/main.go
@@ -24,7 +24,7 @@ var (
 	// DSN is a Data Source Name for Sentry. It is stored in an environment variable and assigned during the build with ldflags.
 	//
 	// See https://docs.sentry.io/product/sentry-basics/dsn-explainer/ for more information.
-	Dsn = os.Getenv("SENTRY_DSN")
+	Dsn string
 	// appVersion value is stored in an environment variable and assigned during the build with ldflags.
 	appVersion string
 )
