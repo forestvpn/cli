3d7ab00ea42518bf8cd32957520e6386c12d8c2a
George Kutsurua <g.kutsurua@gmail.com>
2023-03-09 20:36:39 +0400

    fix: drone

--- utils/ci_setup.sh
+++ utils/ci_setup.sh
@@ -3,5 +3,4 @@ set -eu
 
 GITHUB_TOKEN=${GITHUB_TOKEN:-""}
 
-echo "machine github.com login token password ${GITHUB_TOKEN}" >> ~/.netrc
 git config --global url."https://${GITHUB_TOKEN}@github.com/forestvpn/".insteadOf "https://github.com/forestvpn/"
